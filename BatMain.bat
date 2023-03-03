744d86a6ac1d0ed51beef483ec92b17169ef10c6
ZhangShengKe <1052938743@qq.com>
2023-03-03 13:07:30 +0800

    目录调整

--- BatMain.bat
+++ BatMain.bat
@@ -1,2 +1,2 @@
 ::echo 当前的盘符及路径:%~dp0
-node %~dp0\js\BatMain.js
+node %~dp0\bin\js\BatMain.js
--- bats/js/BuildBase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.BuildBase = void 0;
-var BuildBase = /** @class */ (function () {
-    function BuildBase() {
-    }
-    return BuildBase;
-}());
-exports.BuildBase = BuildBase;
