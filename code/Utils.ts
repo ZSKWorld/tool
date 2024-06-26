8ccfe6f7fb5949d87b112282f0b4a113940da94d
zsk <1052938743@qq.com>
2024-06-26 20:05:08 +0800

    update

--- code/Utils.ts
+++ code/Utils.ts
@@ -70,3 +70,14 @@ export function UpperFirst(str: string, splits?: string[], joinStr = "_") {
         }
     }
 }
+
+export function getDateStr() {
+    const date = new Date();
+    const year = date.getFullYear().toString().padStart(4, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    const hour = date.getHours().toString().padStart(2, "0");
+    const minute = date.getMinutes().toString().padStart(2, "0");
+    const sec = date.getSeconds().toString().padStart(2, "0");
+    return `${ year }/${ month }/${ day } ${ hour }:${ minute }:${ sec }`;
+}
